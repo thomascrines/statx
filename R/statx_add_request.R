5c2257cdd6cb726bb683bb4f63d92c9696cd7e54
Thomas Crines <thomas.crines@hotmail.com>
2019-05-30 08:56:11 +0100

    more changes as result of lintr

--- R/statx_add_request.R
+++ R/statx_add_request.R
@@ -6,9 +6,7 @@
 #' to be called in \code{statx_dataset}.
 #' The \code{request_name} parameter must be passed a user-defined string in order to name the request.
 #' The \code{file_path} parameter must be passed a file path of an existing JSON request file.
-#' JSON files can be written by hand according to the \href{https://stat-xplore.dwp.gov.uk/webapi/online-help/Open-Data-API-Table.html}
-#' {Stat-Xplore guidelines}, but it is easier to create a table using the \href{https://stat-xplore.dwp.gov.uk/webapi/jsf/dataCatalogueExplorer.xhtml}
-#' {Stat-Xplore table generator} and saving the output as \code{Open Data API Query (.json)}.
+#' JSON files can be written by hand according to the \href{https://stat-xplore.dwp.gov.uk/webapi/online-help/Open-Data-API-Table.html}{Stat-Xplore guidelines}, but it is easier to create a table using the \href{https://stat-xplore.dwp.gov.uk/webapi/jsf/dataCatalogueExplorer.xhtml}{Stat-Xplore table generator} and saving the output as \code{Open Data API Query (.json)}.
 #'
 #' @param request_name \code{string}. A user-defined name for the request.
 #' @param file_path \code{string}. The file path of the saved JSON request.
